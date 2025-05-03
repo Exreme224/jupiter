97177e55793d9299122ff4279a7e8ed334c3fad6
Caleb Mickelson <cmickelson95@gmail.com>
2025-05-03 15:18:47 -0500

    added player double jump and fall through platforms

--- scripts/Player.gd
+++ scripts/Player.gd
@@ -5,12 +5,26 @@ extends CharacterBody2D
 @export var gravity := 1300.0
 @export var acceleration := 10.0
 @export var friction := 5.0
+@export var number_of_jumps := 2
+var original_collision_mask: int
+var jumps_left = number_of_jumps
+var was_on_floor = false
+
+func _ready() -> void:
+	original_collision_mask = collision_mask
 
 func _physics_process(delta: float) -> void:
+	
+	was_on_floor = is_on_floor()
 	_handle_movement(delta)
 	_handle_jump()
 	_apply_gravity(delta)
 	move_and_slide()
+	
+	# Reset jumps when player lands
+	if not was_on_floor and is_on_floor():
+		jumps_left = number_of_jumps
+		
 
 func _handle_movement(delta: float) -> void:
 	var input_dir := Input.get_action_strength("move_right") - Input.get_action_strength("move_left")
@@ -21,8 +35,14 @@ func _handle_movement(delta: float) -> void:
 		self.velocity.x = lerp(self.velocity.x, 0.0, friction * delta)
 
 func _handle_jump() -> void:
-	if Input.is_action_just_pressed("jump") and is_on_floor():
+	if Input.is_action_pressed("move_down") and Input.is_action_pressed('jump'):
+		collision_mask = 0
+		jumps_left -= 1
+	elif Input.is_action_just_pressed("jump") and jumps_left > 0:
 		self.velocity.y = -jump_force
+		jumps_left -= 1
+	else:
+		collision_mask = original_collision_mask
 
 func _apply_gravity(delta: float) -> void:
 	if not is_on_floor():
